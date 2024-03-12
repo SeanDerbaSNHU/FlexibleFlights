9aeddf357984fe7c3e19b29cdeb48b746cd538b3
SeanDerba <seanderba@gmail.com>
2024-03-12 01:05:50 -0400

    Update NotificationsFragment.java

--- app/src/main/java/com/example/flexibleflights/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/flexibleflights/ui/notifications/NotificationsFragment.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,19 +20,31 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.flexibleflights.Item;
 import com.example.flexibleflights.MyAdapter;
 import com.example.flexibleflights.R;
 import com.example.flexibleflights.databinding.FragmentNotificationsBinding;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
     List<Item> items = new ArrayList<Item>();
+    final String url = "http://54.163.192.205:3000/search";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -47,11 +60,12 @@ public class NotificationsFragment extends Fragment {
         Button date = root.findViewById(R.id.buttonDate2);
         Button date2 = root.findViewById(R.id.buttonDate3);
         Button passengers = root.findViewById(R.id.buttonNumPassengers2);
+        Button search = root.findViewById(R.id.buttonSearch2);
 
         /////
         //RecyclerView
         /////
-        items = makeDummyList();
+
         RecyclerView recyclerView = root.findViewById(R.id.recyclerView2);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(new MyAdapter(items));
@@ -95,6 +109,45 @@ public class NotificationsFragment extends Fragment {
             }
         });
 
+        date2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //DatePickerFragment newFragment = new DatePickerFragment();
+                //newFragment.show(getParentFragmentManager(), "datePicker");
+                // on below line we are getting
+                // the instance of our calendar.
+                final Calendar c = Calendar.getInstance();
+
+                // on below line we are getting
+                // our day, month and year.
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                // on below line we are creating a variable for date picker dialog.
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        // on below line we are passing context.
+                        view.getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int month, int day) {
+                                // on below line we are setting date to our text view.
+                                month++; //Month returns month-1 for some reason, this fixes it
+                                date2.setText(year + "-" + (month) + "-" + day);
+
+                            }
+                        },
+                        // on below line we are passing year,
+                        // month and day for selected date in our date picker.
+                        year, month, day);
+                // at last we are calling show to
+                // display our date picker dialog.
+                datePickerDialog.show();
+
+            }
+        });
+
         passengers.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -125,6 +178,49 @@ public class NotificationsFragment extends Fragment {
         });
 
 
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RequestQueue queue = Volley.newRequestQueue(root.getContext());
+                HashMap<String, String> params = new HashMap<String, String>();
+                JSONArray array = new JSONArray();
+                JSONObject jsonParam = new JSONObject();
+                try {
+                    jsonParam.put("origin", "LHR");
+                    jsonParam.put("destination", "JFK");
+                    jsonParam.put("departure_date", "2024-03-22");
+                    jsonParam.put("cabin_class", "economy");
+                } catch (JSONException e){
+                    e.printStackTrace();
+                }
+                array.put(jsonParam);
+
+                JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST, url, array, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("RESPONSE", response.toString());
+                        for(int i = 0; i < response.length(); i++){
+                            try {
+                                JSONObject object = response.getJSONObject(i);
+                                items.add(makeItem(object));
+                                Log.d("ITEMS", valueOf(items.size()));
+                                recyclerView.getAdapter().notifyDataSetChanged();
+                            } catch (JSONException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+                //JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST, url, array, )
+                queue.add(jsonArrayRequest);
+            }
+        });
+
         return root;
     }
 
@@ -142,5 +238,28 @@ public class NotificationsFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+    public Item makeItem(JSONObject object) throws JSONException {
+        //Fetch necessary nested JSON objects
+
+        JSONObject slices = object.getJSONArray("slices").getJSONObject(0);
+        JSONObject origin = slices.getJSONObject("origin");
+        JSONObject owner = object.getJSONObject("owner");
+        JSONObject segments = slices.getJSONArray("segments").getJSONObject(0);
+        JSONObject aircraft = segments.getJSONObject("aircraft");
+        JSONObject destination = slices.getJSONObject("destination");
+
+        //Get necessary variables from JSON
+        String price = object.getString("total_amount");
+        String currency = object.getString("total_currency");
+        String name = owner.getString("name");
+        String total_emissions = object.getString("total_emissions_kg");
+        String originName = origin.getString("city_name");
+        String destinationName = destination.getString("city_name");
+        String aircraftName = aircraft.getString("name");
+
+        //Make and return Item object
+        Item item = new Item(price, currency, name, total_emissions, aircraftName, destinationName, originName);
+        return item;
     }
 }
